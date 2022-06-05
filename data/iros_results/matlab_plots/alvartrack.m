110afc049dfc11d9753428657c4a96c3dc41a462
= <=>
2022-06-05 12:57:46 -0400

    Significant cleanup

--- data/iros_results/matlab_plots/alvartrack.m
+++
@@ -1,5 +0,0 @@
-close all;
-y = [1.86, 4.05];
-figure;
-b = bar(y, 'r', 'b');
-% b(1).FaceColor = 'r';
--- scripts/apriltags_rgbd_node.py
+++ scripts/apriltags_rgbd_node.py
@@ -4,8 +4,6 @@
 # Tell python where to find apriltags_rgbd and utils code
 import sys
 import os
-fpath = os.path.join(os.path.dirname(__file__), "apriltags_rgbd")
-sys.path.append(fpath)
 fpath = os.path.join(os.path.dirname(__file__), "utils")
 sys.path.append(fpath)
 
@@ -23,34 +21,21 @@ import tf_utils
 # ROS Imports
 import rospy
 import tf2_ros
-from geometry_msgs.msg import TransformStamped, Vector3, Quaternion, Pose, Point32
-from std_msgs.msg import Bool
+from geometry_msgs.msg import TransformStamped, Vector3
 from sensor_msgs.msg import Image, CameraInfo, PointCloud
 from apriltag_ros.msg import AprilTagDetectionArray
-from message_filters import TimeSynchronizer, Subscriber, ApproximateTimeSynchronizer
+from message_filters import Subscriber, ApproximateTimeSynchronizer
 from cv_bridge import CvBridge, CvBridgeError
-import tf_conversions
 from tf.transformations import quaternion_from_matrix
 from visualization_msgs.msg import MarkerArray, Marker
 
 # Custom messages
 from apriltags_rgbd.msg import PointArray, LabeledPointArray
 
-# For filtering
-ENABLE_FILTER = True
-from tag_detection_filter import TagDetectionFilter
-
-# TODO: Remove hardcode
-TAG_PREFIX = ""#"detected_"
-
 class ApriltagsRgbdNode():
     def __init__(self):
         rospy.init_node("apriltags_rgbd")
         self.rate = rospy.Rate(60) # 10 Hz
-
-        # Load ROS parameters
-        self.rgbd_pos_flag = rospy.get_param("~use_rgbd_position")
-        self.rgbd_rot_flag = rospy.get_param("~use_rgbd_rotation")
 
         # CV bridge
         self.bridge = CvBridge()
@@ -68,10 +53,6 @@ class ApriltagsRgbdNode():
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
         self.tf_broadcaster = tf2_ros.TransformBroadcaster()
 
-        # Point filtering
-        if ENABLE_FILTER:
-            self.filter = TagDetectionFilter()
-
         # Publishers
         self.tag_tf_pub = rospy.Publisher("/apriltags_rgbd/tag_tfs", TransformStamped, queue_size=10)
         self.tag_pt_pub = rospy.Publisher("/apriltags_rgbd/extracted_tag_pts", PointCloud, queue_size=10)
@@ -85,8 +66,6 @@ class ApriltagsRgbdNode():
             rgb_data = rgb_data
             depth_data = depth_data
             tag_data = tag_data
-
-            # TODO: check timestamps here
 
             # Convert ROS images to OpenCV frames
             try:
@@ -116,49 +95,10 @@ class ApriltagsRgbdNode():
 
                 depth_pts = self.extractDepthPoints(depth_image, image_pts, k_mtx)
 
-                # try:
-                #     # Fit plane and compute corner positions
-                #     depth_plane_est, all_pts = fuse.sample_depth_plane(depth_image, image_pts, k_mtx)
-                #     depth_points = fuse.getDepthPoints(image_pts, depth_plane_est, depth_image, k_mtx)
-                # except:
-                #     rospy.logwarn("Error in plane fitting - skipping tag " + str(tag_id))
-                #     continue
-
-                # print(len(all_pts))
-                # # Plane Normal Vector
-                # n_vec = -depth_plane_est.mean.n # Negative because plane points
-                #                                 # in opposite direction
-
-                # # TODO: left off here - this filter results in really clean detections, but is too aggressive
-                # if ENABLE_FILTER:
-                #     # Filter points
-                #     depth_points, n_vec = self.filter.updateEstimate(tag_id, depth_points, n_vec)
-
-                #     # Check results from filter
-                #     if depth_points == None:
-                #         rospy.loginfo("invalid depth points")
-                #         continue
-                    
-                #     if np.isnan(np.sum(n_vec)):
-                #         rospy.loginfo("malformed normal vector")
-                #         continue
-
-                # Compute pose
-                # center_pt, quat = self.computePoseFromDepth(depth_points, n_vec)
-
-
-                # TODO: figure out why this piece of code causes jumpy pose estimates
-                # # Override pose with detection based on camera if necessary
-                # cam_center_pt, cam_quat = self.extractPoseFromMsg(tag)
-                # if not self.rgbd_pos_flag:
-                #     center_pt = cam_center_pt 
-                # if not self.rgbd_rot_flag:
-                #     quat = cam_quat
-
                 # Update tf tree
                 output_tf = TransformStamped()
                 output_tf.header = header
-                output_tf.child_frame_id = TAG_PREFIX + str(tag_id)
+                output_tf.child_frame_id = str(tag_id)
 
                 # Estimate tag position based on average depth measurement
                 if len(depth_pts) == 0:
@@ -171,44 +111,6 @@ class ApriltagsRgbdNode():
 
                 self.tf_broadcaster.sendTransform(output_tf)
                 self.tag_tf_pub.publish(output_tf)
-
-                # # Save tag and corner points
-                # corner_pts_tag = PointArray()
-
-                # for i in range(len(all_pts)):
-                #     tag_pts.points.append(Point32(*all_pts[i]))
-
-                # for i in range(len(depth_points)):
-                #     corner_pts.points.append(Point32(*depth_points[i]))
-                #     corner_pts_tag.points.append(Point32(*depth_points[i]))
-
-                # corner_pts_labeled_array.labels.append(str(tag_id))
-                # corner_pts_labeled_array.point_arrays.append(corner_pts_tag)
-
-                # # Create visualization markers
-                # marker = Marker()
-                # marker.header = header
-                # marker.id = int(tag_id)
-                # marker.type = 0
-                # marker.pose.position = Point32(0,0,0)
-                # marker.pose.orientation = Quaternion(0,0,0,1)
-                # marker.color.r = 1.0
-                # marker.color.g = 0.0
-                # marker.color.b = 0.0
-                # marker.color.a = 1.0
-                # marker.scale.x = 0.005
-                # marker.scale.y = 0.01
-                # marker.scale.z = 0.0
-                # pt1 = Point32(*center_pt)
-                # pt2 = Point32(*(center_pt + n_vec/5))
-                # marker.points = [pt1, pt2]
-                # marker_array_msg.markers.append(marker)
-
-            # Publish data
-            # self.tag_pt_pub.publish(tag_pts)
-            # self.corner_pt_pub.publish(corner_pts)
-            # self.corner_pt_labeled_pub.publish(corner_pts_labeled_array)
-            # self.marker_arr_pub.publish(marker_array_msg)
 
             self.rate.sleep()
 
@@ -239,51 +141,6 @@ class ApriltagsRgbdNode():
         samples_depth = np.array(all_pts)
         return samples_depth
 
-    def loop(self):
-        self.rate.sleep()
-
-    def computePoseFromDepth(self, depth_points, n_vec):
-        # Compute center of plane
-        center_pt = np.mean(depth_points, axis=0)
-
-        # Compute magnitude of normal
-        n_norm = np.linalg.norm(n_vec)
-
-        # Compute point in direction of x-axis
-        x_pt = (depth_points[1] + depth_points[2]) / 2
-
-        # Compute first orthogonal vector - project point onto plane
-        u = x_pt - center_pt
-        v = n_vec
-        v_norm = n_norm
-        x_vec = u - (np.dot(u, v)/v_norm**2)*v
-
-        # Compute second orthogonal vector - take cross product
-        y_vec = np.cross(n_vec, x_vec)
-
-        # Normalize vectors
-        x_vec = x_vec / np.linalg.norm(x_vec)
-        y_vec = y_vec / np.linalg.norm(y_vec)
-        n_vec = n_vec / np.linalg.norm(n_vec)
-
-        # TODO: Optimize this
-        x_vec1 = list(x_vec) + [0]
-        y_vec1 = list(y_vec) + [0]
-        n_vec1 = list(n_vec) + [0]
-
-        R_t = np.array([x_vec1,y_vec1,n_vec1,[0,0,0,1]])
-
-        quat = quaternion_from_matrix(R_t.transpose()) # [q_x, q_y, q_z, q_w]
-
-        # Normalize quaternion
-        quat = quat / np.linalg.norm(quat)
-        return center_pt, quat
-
-    def extractPoseFromMsg(self, detection):           
-        pose = detection.pose.pose.pose
-        center_pt, quat = tf_utils.pose_to_pq(pose)
-        return center_pt, quat
-
     def parseTag(self, tag):
         id = str(tag.id[0])
 
@@ -297,20 +154,8 @@ class ApriltagsRgbdNode():
 
         return id, image_pts
 
-    # def composeTfMsg(self, trans, q, header, tag_id):
-    #     output_tf = TransformStamped()
-
-    #     # Update header info
-    #     output_tf.header = header
-    #     output_tf.child_frame_id = TAG_PREFIX + str(tag_id)
-
-    #     # Populate translation info
-    #     output_tf.transform.translation = Vector3(*trans)
-
-    #     # Populate rotation info
-    #     output_tf.transform.rotation = Quaternion(*q)
-
-    #     return output_tf
+    def loop(self):
+        self.rate.sleep()
 
 if __name__ == '__main__':
     node = ApriltagsRgbdNode()
